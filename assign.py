6222d2a2911f1fcddc44b5914b3007fec6d139d2
Jim <mainprice@gmail.com>
2019-05-07 11:52:41 +0200

    Add assignment

--- assign.py
+++ assign.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python
-# This code solves linear sum assignment problem, also
-# known as minimum weight matching in bipartite graphs
-# See the assignment problem
-# https://en.wikipedia.org/wiki/Assignment_problem
+
+# Copyright (c) 2010, University of Stuttgart
+# All rights reserved.
+#
+# Permission to use, copy, modify, and distribute this software for any purpose
+# with or without   fee is hereby granted, provided   that the above  copyright
+# notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS  SOFTWARE INCLUDING ALL  IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR  BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR  ANY DAMAGES WHATSOEVER RESULTING  FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION,   ARISING OUT OF OR IN    CONNECTION WITH THE USE   OR
+# PERFORMANCE OF THIS SOFTWARE.
+#
+#                                        Jim Mainprice on Sunday May 7 2019
+
+# This code solves the linear sum assignment problem, also known as minimum
+# weight matching in bipartite graphs
+
+# See the assignment problem:
+# [Wikipedia](https://en.wikipedia.org/wiki/Assignment_problem)
+
 # It is useful for assigning jobs to students depending
 # on preferences. You can simply define the name and
 # preferences in a yaml file.
-# WARNING the indices start with one in the yaml file
-# and 0 in the code.
+# WARNING the indices start with one in the yaml file and 0 in the code.
 
 import yaml
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 
-# file = "example.yaml",
-file = "students.yaml"
+file = "example.yaml",
 with open(file, 'r') as stream:
     try:
         settings = yaml.safe_load(stream)
